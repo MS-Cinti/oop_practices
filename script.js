24045a20914d601a5bc7276463d0a000524b8b61
MS-Cinti <szabadicintia@gmail.com>
2022-04-11 14:32:57 +0200

    added class, constructor

---
+++ script.js
@@ -0,0 +1,9 @@
+//létrehozzuk a függvényt
+//példányosítás folyamata
+
+function Poem(author, year, title, text) {
+    this.author = author;
+    this.year = year;
+    this.title = title;
+    this.text = text;
+}
