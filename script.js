2c276430b7203a629a8b3d462ad513b68569f9e4
MS-Cinti <szabadicintia@gmail.com>
2022-04-11 14:38:48 +0200

    added example

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 //létrehozzuk a függvényt
-//példányosítás folyamata
-
 function Poem(author, year, title, text) {
     this.author = author;
     this.year = year;
     this.title = title;
     this.text = text;
+}
+
+//példányosítás folyamata
+const poemData = {
+    authorFromInternet: "Weöres Sándor",
+    yearFromInternet: 1962,
+    titleFromInternet: "Énhasadás",
+    textFromInternet: "Utcasarkon várok rám, félórája is van tán. Ám értelmem fölragyog Nem jövök, mert itt vagyok."
 }
